01f6199184ae7fd28d812a24a9352c9f4f1e89f0
Cristi_Sima <cristiansimache@gmail.com>
2024-04-07 15:26:59 +0300

    + backend ssr base + main page + item list * docker compose fixes

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,31 @@
 from flask import Flask, render_template, request, Blueprint
 from documents import *
+from init_db import init_db
 import mongoengine
 import time
 
 app = Flask(__name__)
 
+@app.context_processor
+def renderers():
+    return {
+        "round": round,
+        "zip": zip,
+        "list": list,
+        "enumerate": enumerate,
+        "print": print,
+        "len": len,
+    }
+
+@app.get("/items")
+def get_items():
+    return render_template("items.html.j2", items=Item.objects())
+
 
 @app.get("/")
 def test():
-    return ""
+    return render_template("home.html.j2")
+
 
 if __name__ == '__main__':
     time.sleep(5)
@@ -17,5 +34,5 @@ if __name__ == '__main__':
         print("DB Empty, Initialising..", flush=True)
         init_db()
         print("Initialisation:\t COMPLETED", flush=True)
-        
+
     app.run(host="0.0.0.0", debug=False)
